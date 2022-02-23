9f07cbbcf9370ba4b37c4d9822410673b4192772
CEPL <CEPL@DESKTOP-PMA58JT>
2022-02-23 15:34:31 +0530

    Another branch

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Tested revert now testing mering");
 		System.out.println("Testing rebase");
+		System.out.println("Another branch");
 		
 	}
 }
