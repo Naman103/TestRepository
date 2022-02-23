2e02337fe619e1722cd369c28a7530a7d4386186
CEPL <CEPL@DESKTOP-PMA58JT>
2022-02-23 15:24:28 +0530

    Resolving errors

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Tested revert now testing mering");
 		System.out.println("Testing rebase");
-		System.out.println("Checking changes in branch1");
+		
 	}
 }
